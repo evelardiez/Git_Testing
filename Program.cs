44e0628b3eacf0cae50b6cd323c6634499e87481
Emilio Velardiez <emilio.velardiez_moreno@roche.com>
2020-06-08 22:37:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Roche">
+//   Roche Corporation
+// </copyright>
+// <summary>
+//   Console test project.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Git_Testing
+{
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
