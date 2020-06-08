e4517623ec1349ceb7eca121a5d828d3f9a9da9f
Emilio Velardiez <emilio.velardiez_moreno@roche.com>
2020-06-08 22:48:21 +0200

    Cosmetic clean up

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Roche">
+//   Roche Corporation
+// </copyright>
+// <summary>
+//   Console test project.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Git_Testing
 {
-    class Program
+    using System;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// The main.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
